7aa7bba967c735a4925feb43fcd8ba82318e481f
Mohsen Iranmanesh <mhsn.iranmanesh@gmail.com>
2019-02-27 07:50:22 +0330

    depoloy setting implemented

--- qrgo/settings.py
+++ qrgo/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'qrgo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'qrgame',
+        'USER': 'mohsen',
+        'PASSWORD': 'Pdnejoh123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -119,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
